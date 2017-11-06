4dc59abfeea1232047d21802140abe5010099480
Dan Batinas <batinasdangmail.com>
2017-11-06 15:56:58 +0000

    Some changes added

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func handleFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1> Welcome to Go lang website</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handleFunc)
+	http.ListenAndServe(":3000", nil)
+
+}
